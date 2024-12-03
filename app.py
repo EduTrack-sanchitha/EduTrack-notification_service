fbf291bd4a632c34a02aa7065588eb74d891a245
Chathushka <udanasanchitha4@gmail.com>
2024-12-03 12:41:28 +0530

    notofication

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+db_config = {
+    "host": "localhost",
+    "port": "8889",
+    "user": "root",
+    "password": "root",
+    "database": "edutrack"
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/notifications', methods=['GET'])
+def get_schedules():
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM schedules")
+    schedules = cursor.fetchall()
+    connection.close()
+    return jsonify(schedules)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)
